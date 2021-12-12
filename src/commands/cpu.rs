daf5b77861223827f35c83644a73fccf73a58a05
Louis Bailleau <louis.bailleau@free.fr>
2021-12-12 16:17:42 +0100

    Show some system informations

--- src/commands/cpu.rs
+++ src/commands/cpu.rs
@@ -1,11 +1,23 @@
 use billboard::Billboard;
 use clap::{App, ArgMatches};
+use sysinfo::SystemExt;
 
 pub fn cpu_info_subcommand() -> App<'static> {
     App::new("pcinfo").about("Show informatoins of your computer")
 }
 
 pub fn cpu_info_handler(_args: &ArgMatches) {
-    let txt: String = format!("ARCH: {}", "lol");
+    let mut system = sysinfo::System::new_all();
+    system.refresh_all();
+
+    let none_str = String::from("none");
+    let arch = system.name().unwrap_or(none_str.clone());
+    let platform = system.os_version().unwrap_or(none_str.clone());
+    let version = system.os_version().unwrap_or(none_str.clone());
+
+    let txt: String = format!(
+        "ARCH: {}\nPlatform: {}\nVersion: {}\n",
+        arch, platform, version,
+    );
     Billboard::default().display(txt.as_str())
 }
